dd9ff4ba328f476d863ca88467fd49f141e82420
Jae Kim <jaekim@Jaeui-MacBookPro.local>
2021-02-13 21:25:25 +0900

    Minor Fix to Header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -150,48 +150,42 @@ export default function Header(props) {
     setSelectedIndex(i);
   };
 
-  const menuOptions = useMemo(
-    () => [
-      { name: "Services", link: "/services", activeIndex: 1, selectedIndex: 0 },
-      {
-        name: "Custom Software",
-        link: "/customsoftware",
-        activeIndex: 1,
-        selectedIndex: 1,
-      },
-      {
-        name: "Mobile App Development",
-        link: "/mobileapps",
-        activeIndex: 1,
-        selectedIndex: 2,
-      },
-      {
-        name: "Website Development",
-        link: "/websites",
-        activeIndex: 1,
-        selectedIndex: 3,
-      },
-    ],
-    []
-  );
+  const menuOptions = [
+    { name: "Services", link: "/services", activeIndex: 1, selectedIndex: 0 },
+    {
+      name: "Custom Software",
+      link: "/customsoftware",
+      activeIndex: 1,
+      selectedIndex: 1,
+    },
+    {
+      name: "Mobile App Development",
+      link: "/mobileapps",
+      activeIndex: 1,
+      selectedIndex: 2,
+    },
+    {
+      name: "Website Development",
+      link: "/websites",
+      activeIndex: 1,
+      selectedIndex: 3,
+    },
+  ];
 
-  const routes = useMemo(
-    () => [
-      { name: "Home", link: "/", activeIndex: 0 },
-      {
-        name: "Services",
-        link: "/services",
-        activeIndex: 1,
-        ariaOwns: anchorEl ? "simple-menu" : undefined,
-        ariaPopup: anchorEl ? "true" : undefined,
-        mouseOver: (event) => handleClick(event),
-      },
-      { name: "Revolution", link: "/revolution", activeIndex: 2 },
-      { name: "About Us", link: "/about", activeIndex: 3 },
-      { name: "Contact Us", link: "/contact", activeIndex: 4 },
-    ],
-    []
-  );
+  const routes = [
+    { name: "Home", link: "/", activeIndex: 0 },
+    {
+      name: "Services",
+      link: "/services",
+      activeIndex: 1,
+      ariaOwns: anchorEl ? "simple-menu" : undefined,
+      ariaPopup: anchorEl ? "true" : undefined,
+      mouseOver: (event) => handleClick(event),
+    },
+    { name: "Revolution", link: "/revolution", activeIndex: 2 },
+    { name: "About Us", link: "/about", activeIndex: 3 },
+    { name: "Contact Us", link: "/contact", activeIndex: 4 },
+  ];
 
   useEffect(() => {
     [...menuOptions, ...routes].forEach((route) => {
